ccf352618425cb2f369b1478e402c740dd387e4a
Wilson Gearld Mead III <kem@rangoric.com>
2012-07-13 20:57:55 -0400

    updated JSON.Net

--- RangoricMUD/RangoricMUD/Global.asax.cs
+++ RangoricMUD/RangoricMUD/Global.asax.cs
@@ -22,8 +22,6 @@ using System.Web.Routing;
 
 namespace RangoricMUD
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
     public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
