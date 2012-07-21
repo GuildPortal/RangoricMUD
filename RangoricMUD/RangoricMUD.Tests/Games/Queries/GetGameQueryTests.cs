3d35782bfeb6f233a507866a4b2627edf6226d50
Wilson Gearld Mead III <kem@rangoric.com>
2012-07-21 09:44:54 -0400

    Get Singular Named Game Query Done
    Will load the game with the givin name.

---
+++ RangoricMUD/RangoricMUD.Tests/Games/Queries/GetGameQueryTests.cs
@@ -0,0 +1,48 @@
+﻿#region License
+
+// RangoricMUD is licensed under the Open Game License.
+// The original code and assets provided in this repository are Open Game Content,
+// The name RangoricMUD is product identity, and can only be used as a part of the code,
+//   or in reference to this project.
+// 
+// More details and the full text of the license are available at:
+//   https://github.com/Rangoric/RangoricMUD/wiki/Open-Game-License
+// 
+// RangoricMUD's home is at: https://github.com/Rangoric/RangoricMUD
+
+#endregion
+
+#region References
+
+using NUnit.Framework;
+using RangoricMUD.Games.Models;
+using RangoricMUD.Games.Queries;
+using RangoricMUD.Tests.Utilities;
+
+#endregion
+
+namespace RangoricMUD.Tests.Games.Queries
+{
+    [TestFixture]
+    public class GetGameQueryTests : BaseTests
+    {
+        [TestCase("ABC", new[] {"ABC", "BCD", "CDE"})]
+        [TestCase("BCD", new[] {"ABC", "BCD", "CDE"})]
+        [TestCase("CDE", new[] {"ABC", "BCD", "CDE"})]
+        public void GetGameFromDocumentStore(string tGameName, string[] tGameList)
+        {
+            var vDatabase = GetEmbeddedDatabase;
+
+            foreach (var vGameName in tGameList)
+            {
+                AddGameToDatabase(vGameName, vDatabase);
+            }
+
+            var vModel = new GetGameModel {Name = tGameName};
+
+            var vQuery = new GetGameQuery(vDatabase, vModel);
+
+            Assert.AreEqual(tGameName, vQuery.Result.Name);
+        }
+    }
+}
---
+++ RangoricMUD/RangoricMUD/Games/Queries/GetGameQuery.cs
@@ -0,0 +1,60 @@
+﻿#region License
+
+// RangoricMUD is licensed under the Open Game License.
+// The original code and assets provided in this repository are Open Game Content,
+// The name RangoricMUD is product identity, and can only be used as a part of the code,
+//   or in reference to this project.
+// 
+// More details and the full text of the license are available at:
+//   https://github.com/Rangoric/RangoricMUD/wiki/Open-Game-License
+// 
+// RangoricMUD's home is at: https://github.com/Rangoric/RangoricMUD
+
+#endregion
+
+#region References
+
+using RangoricMUD.Games.Data;
+using RangoricMUD.Games.Models;
+using RangoricMUD.Queries;
+using Raven.Client;
+
+#endregion
+
+namespace RangoricMUD.Games.Queries
+{
+    public interface IGetGameQuery : IQuery<Game>
+    {
+    }
+
+    public class GetGameQuery : IGetGameQuery
+    {
+        private readonly IDocumentStore mDocumentStore;
+        private readonly GetGameModel mGetGameModel;
+
+        public GetGameQuery(IDocumentStore tDocumentStore, GetGameModel tGetGameModel)
+        {
+            mDocumentStore = tDocumentStore;
+            mGetGameModel = tGetGameModel;
+        }
+
+        #region IGetGameQuery Members
+
+        public Game Result
+        {
+            get
+            {
+                Game vResult;
+
+                using (var vSession = mDocumentStore.OpenSession())
+                {
+                    vResult = vSession.Load<Game>(mGetGameModel.Name);
+                }
+
+                return vResult;
+            }
+        }
+
+        #endregion
+    }
+}
