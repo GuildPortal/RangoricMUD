633902e96fe068791f766ed3de47cda774168c17
Wilson Gearld Mead III <kem@rangoric.com>
2012-08-21 19:28:07 -0400

    Gettting closer
    ...

--- RangoricMUD/RangoricMUD/Assets/Scripts/Components/Controllers.js
+++ RangoricMUD/RangoricMUD/Assets/Scripts/Components/Controllers.js
@@ -3,6 +3,7 @@
     return {
         Home: HomeController(tDependencies),
         Accounts: AccountsController(tDependencies),
-        Games: GamesController(tDependencies)
+        Games: GamesController(tDependencies),
+        Play: PlayController(tDependencies)
     };
 }
---
+++ RangoricMUD/RangoricMUD/Assets/Scripts/Games/PlayController.js
@@ -0,0 +1,13 @@
+﻿function PlayController(tDependencies) {
+    return {
+        Start: function () {
+            var vGame = arguments[0];
+            return {
+                Page: "Play/Start",
+                IsVisible: ko.computer(function() {
+                    return tDependencies.AccountManager.IsLoggedIn();
+                })
+            };
+        }  
+    };
+}
