1f02f7ef15cd12135c20844472623a40a32e01b6
Wilson Gearld Mead III <kem@rangoric.com>
2012-08-30 07:23:45 -0700

    starting service statuses

---
+++ RangoricMUD/RangoricMUD/Assets/Scripts/Pages/ServerStatusManager.js
@@ -0,0 +1,12 @@
+﻿function ServerStatusManager(tDependencies) {
+    var vThis = {
+        IsRavenConnected: ko.observable(false),
+        CheckStatus: function () {
+            tDependencies.Connection.serverStatus.startStatusCheck();
+        }
+    };
+    tDependencies.Connection.serverStatus.UpdateRavenStatus = function(tData) {
+        vThis.IsRavenConnected(tData);
+    };
+    return vThis;
+}
---
+++ RangoricMUD/RangoricMUD/ServerStatus/Controllers/ServerStatusHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SignalR.Hubs;
+
+namespace RangoricMUD.ServerStatus.Controllers
+{
+    public class ServerStatusHub : Hub
+    {
+        public void StartStatusCheck()
+        {
+            
+        }
+    }
+}
